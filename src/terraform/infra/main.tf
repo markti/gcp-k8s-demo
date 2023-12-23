6135c881b8680fe0e02306a9524a3726c596b686
markti <markti@outlook.com>
2023-12-23 13:38:09 -0600

    add a random_string

--- src/terraform/infra/main.tf
+++ src/terraform/infra/main.tf
@@ -1,5 +1,10 @@
 resource "google_project" "main" {
   name       = "${var.application_name}-${var.environment_name}"
-  project_id = "${var.application_name}-${var.environment_name}"
+  project_id = "${var.application_name}-${var.environment_name}-${random_string.project_id.result}"
   org_id     = var.gcp_organization
+}
+
+resource "random_string" "project_id" {
+  length  = 8
+  special = false
 }

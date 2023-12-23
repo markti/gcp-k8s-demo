020bd04c65bbb47a358a0a0b179db610d91aa38e
markti <markti@outlook.com>
2023-12-23 16:44:23 -0600

    add container registry

---
+++ src/terraform/infra/services.tf
@@ -0,0 +1,11 @@
+resource "google_project_service" "container" {
+  project = google_project.main.project_id
+  service = "containerregistry.googleapis.com"
+
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+
+  disable_dependent_services = true
+}

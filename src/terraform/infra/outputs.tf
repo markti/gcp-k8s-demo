14536c1837283102cbedadb4d5f56195ffd8d8d7
markti <markti@outlook.com>
2024-01-15 18:53:05 -0500

    get cluster outputs

--- src/terraform/infra/outputs.tf
+++ src/terraform/infra/outputs.tf
@@ -4,3 +4,19 @@ output "project_id" {
 output "current_user" {
   value = data.google_client_openid_userinfo.provider_identity.email
 }
+output "host" {
+  value     = google_container_cluster.primary.endpoint
+  sensitive = true
+}
+output "client_certificate" {
+  value     = google_container_cluster.primary.master_auth.0.client_certificate
+  sensitive = true
+}
+output "client_key" {
+  value     = google_container_cluster.primary.master_auth.0.client_key
+  sensitive = true
+}
+output "cluster_ca_certificate" {
+  value     = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
+  sensitive = true
+}

725efff313c71a1ff0d7f76e2b237acac1ea4e26
markti <markti@outlook.com>
2024-01-14 23:08:24 -0500

    add project_id

--- src/terraform/infra/cluster.tf
+++ src/terraform/infra/cluster.tf
@@ -6,9 +6,9 @@ resource "google_service_account" "cluster" {
 
 
 resource "google_container_cluster" "main" {
+  project  = google_project.main.project_id
   name     = "gke-${var.application_name}-${var.environment_name}-${random_string.project_id.result}"
   location = var.primary_region
-
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
@@ -17,6 +17,8 @@ resource "google_container_cluster" "main" {
 }
 
 resource "google_container_node_pool" "primary" {
+
+  project    = google_project.main.project_id
   name       = "gke-${var.application_name}-${var.environment_name}-${random_string.project_id.result}-primary"
   location   = var.primary_region
   cluster    = google_container_cluster.main.name

dec649eba84480f0f1fa8ddceef7bcd056ec2eb7
markti <markti@outlook.com>
2024-01-14 20:00:34 -0500

    billing account

--- src/terraform/infra/main.tf
+++ src/terraform/infra/main.tf
@@ -1,7 +1,12 @@
+data "google_billing_account" "main" {
+  display_name = "Default"
+}
+
 resource "google_project" "main" {
-  name       = "${var.application_name}-${var.environment_name}"
-  project_id = "${var.application_name}-${var.environment_name}-${random_string.project_id.result}"
-  org_id     = var.gcp_organization
+  name            = "${var.application_name}-${var.environment_name}"
+  project_id      = "${var.application_name}-${var.environment_name}-${random_string.project_id.result}"
+  org_id          = var.gcp_organization
+  billing_account = data.google_billing_account.main.id
 }
 
 resource "random_string" "project_id" {

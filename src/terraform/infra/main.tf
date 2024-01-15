41983275349d7a8ab262019570596b14cf9e548d
markti <markti@outlook.com>
2024-01-14 20:26:12 -0500

    set billing account

--- src/terraform/infra/main.tf
+++ src/terraform/infra/main.tf
@@ -9,10 +9,10 @@ data "google_client_openid_userinfo" "provider_identity" {
 }
 
 resource "google_project" "main" {
-  name       = "${var.application_name}-${var.environment_name}"
-  project_id = "${var.application_name}-${var.environment_name}-${random_string.project_id.result}"
-  org_id     = var.gcp_organization
-  //billing_account = data.google_billing_account.main.id
+  name            = "${var.application_name}-${var.environment_name}"
+  project_id      = "${var.application_name}-${var.environment_name}-${random_string.project_id.result}"
+  org_id          = var.gcp_organization
+  billing_account = data.google_billing_account.main.id
 }
 
 resource "random_string" "project_id" {

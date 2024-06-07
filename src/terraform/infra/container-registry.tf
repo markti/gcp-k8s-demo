82f15c7e1c0fc50a2513e80102dbcf523f56e0c4
markti <markti@outlook.com>
2024-06-07 01:15:27 -0400

    fix names

--- src/terraform/infra/container-registry.tf
+++ src/terraform/infra/container-registry.tf
@@ -1,13 +1,13 @@
 resource "google_artifact_registry_repository" "frontend" {
   project       = google_project.main.project_id
   location      = var.primary_region
-  repository_id = "${var.application_name}-${var.environment_name}-${random_string.project_id.result}-frontend"
+  repository_id = "frontend"
   format        = "DOCKER"
 }
 
 resource "google_artifact_registry_repository" "backend" {
   project       = google_project.main.project_id
   location      = var.primary_region
-  repository_id = "${var.application_name}-${var.environment_name}-${random_string.project_id.result}-backend"
+  repository_id = "backend"
   format        = "DOCKER"
 }

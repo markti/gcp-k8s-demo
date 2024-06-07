bae6e0eabdbf310bb7a7eae5697b23edb4a9e651
markti <markti@outlook.com>
2024-06-07 01:13:36 -0400

    artifacts

--- src/terraform/infra/container-registry.tf
+++ src/terraform/infra/container-registry.tf
@@ -1,10 +1,12 @@
 resource "google_artifact_registry_repository" "frontend" {
+  project       = google_project.main.project_id
   location      = var.primary_region
   repository_id = "${var.application_name}-${var.environment_name}-${random_string.project_id.result}-frontend"
   format        = "DOCKER"
 }
 
 resource "google_artifact_registry_repository" "backend" {
+  project       = google_project.main.project_id
   location      = var.primary_region
   repository_id = "${var.application_name}-${var.environment_name}-${random_string.project_id.result}-backend"
   format        = "DOCKER"

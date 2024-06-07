b305f2635d44956f26c2b83a43b239a262f72a98
markti <markti@outlook.com>
2024-06-07 01:24:09 -0400

    repository urls

--- src/terraform/infra/outputs.tf
+++ src/terraform/infra/outputs.tf
@@ -10,3 +10,9 @@ output "kubernetes_cluster_name" {
 output "primary_region" {
   value = var.primary_region
 }
+output "frontend_registry" {
+  value = "${var.primary_region}-docker.pkg.dev/${google_project.main.project_id}/${google_artifact_registry_repository.frontend.name}"
+}
+output "backend_registry" {
+  value = "${var.primary_region}-docker.pkg.dev/${google_project.main.project_id}/${google_artifact_registry_repository.frontend.name}"
+}

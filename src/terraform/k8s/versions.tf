420b80ad560215c2bc15602476b5934c00a3cd84
markti <markti@outlook.com>
2024-06-07 00:01:23 -0400

    k8s provider init

--- src/terraform/k8s/versions.tf
+++ src/terraform/k8s/versions.tf
@@ -24,13 +24,11 @@ data "google_container_cluster" "main" {
   location = var.primary_region
 }
 
-/*
 provider "kubernetes" {
 
-  host                   = var.host
-  client_certificate     = var.client_certificate
-  client_key             = var.client_key
-  cluster_ca_certificate = var.cluster_ca_certificate
+  host                   = data.google_container_cluster.main.endpoint
+  client_certificate     = data.google_container_cluster.main.master_auth.0.client_certificate
+  client_key             = data.google_container_cluster.main.master_auth.0.client_key
+  cluster_ca_certificate = data.google_container_cluster.main.master_auth.0.cluster_ca_certificate
 
 }
-*/

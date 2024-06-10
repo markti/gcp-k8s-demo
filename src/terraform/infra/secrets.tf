ad0dcf13361b50d06cac63c6b0504de75fbdfa8f
markti <markti@outlook.com>
2024-06-10 11:57:13 +1000

    secret store gcp

--- src/terraform/infra/secrets.tf
+++ src/terraform/infra/secrets.tf
@@ -11,3 +11,15 @@ resource "helm_release" "csi_secrets_store" {
   }
 
 }
+
+
+resource "helm_release" "gcp_secrets_provider" {
+
+  name       = "secrets-store-csi-driver-provider-gcp"
+  repository = "https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts"
+  chart      = "secrets-store-csi-driver-provider-gcp"
+  namespace  = "kube-system"
+
+}
+
+#helm upgrade --install secrets-store-csi-driver-provider-gcp charts/secrets-store-csi-driver-provider-gcp

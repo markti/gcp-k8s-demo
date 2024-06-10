93505d49be78b08565bcdf70d63ffe0a1a0be0b0
markti <markti@outlook.com>
2024-06-10 13:35:10 +1000

    secrets

--- src/terraform/infra/services.tf
+++ src/terraform/infra/services.tf
@@ -23,3 +23,16 @@ resource "google_project_service" "container" {
 
   disable_dependent_services = true
 }
+
+resource "google_project_service" "secrets" {
+
+  project = google_project.main.project_id
+  service = "secretmanager.googleapis.com"
+
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+
+  disable_dependent_services = true
+}
